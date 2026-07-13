a7ee713b7cc50784e29167eab7fde1ad6ee40371
Sarath Kumar Ezilarasan <sarathezil@gmail.com>
2026-07-13 20:02:08 +0530

    day-1

---
+++ core-java/src/_1_Datatypes.java
@@ -0,0 +1,35 @@
+public class _1_Datatypes {
+
+    //  skdkjsdjfk sdlfkslkdflk dslsdkflkxdlf
+
+    // main method or main function
+    public static void main(String[] args) {
+        // variables -> x = 100 , y = 200 , x+y = 300
+        // keywords -> data types -> primitive , non primitive -> String
+        // byte  -> whole number  -> 1 bytes -> -128 to 127
+        // short -> whole number ->  2 bytes -> -32k to 32k
+        // int    -> whole number -> 4 bytes -> -2*10^9 to 2*10^9
+        // long   -> whole number -> 8 bytes -> -2*10^63 to 2*10^63
+        // float  -> decimal number -> 4 bytes -> 5 decimal digits
+        // double -> decimal number -> 8 bytes -> 16 decimal digits
+        // boolean
+        // char
+        // String
+
+//        char i = 'a';
+//        String x = "";
+//        System.out.println(x);
+
+        // scope of a variable
+//        System.out.println(x);
+//        int x = 100;
+//        x = 200;
+
+//        System.out.println(x);
+
+
+
+    }
+}
+
+// heap , stack
---
+++ core-java/src/_2_TypeCasting.java
@@ -0,0 +1,23 @@
+public class _2_TypeCasting {
+    public static void main(String[] args) {
+        // byte -> short -> int -> long -> float -> double [up casting]
+
+        // long -> int [down casting]
+
+
+//        int x = 100;
+//        long l = x;
+//        long l = 100000000000l;
+//        System.out.println(l);
+//
+//        x = (int) l;
+//        System.out.println(x);
+
+
+        long l = 100000000000l;
+        float f = l;
+
+        System.out.println(f);
+
+    }
+}
